116ff75c3bbf8e8f7bbc32431a732ec8d0474e26
Martin Enke <trickform.info@gmail.com>
2025-03-10 18:10:01 +0100

    added load_dotenv() & os.getenv()

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,4 +1,7 @@
 import requests
+import os
+from dotenv import load_dotenv
+
 
 def fetch_data(animal):
     """
@@ -17,7 +20,13 @@ def fetch_data(animal):
     }
     },
     """
-    API_KEY = "Sxt9ncDQfyLYr5YnqVXY3w==AiT3mSC1ktGYGWbp"
+    load_dotenv()
+    API_KEY = os.getenv('API_KEY')
+
+    if not API_KEY:
+        print("Error: API_KEY not found!")
+        return []
+
     api_url = f'https://api.api-ninjas.com/v1/animals?name={animal}'
     response = requests.get(api_url, headers={"X-API-KEY": API_KEY})
 
